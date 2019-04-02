c24c4d3dcd95c61886baa20329bc607a3922956b
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-04-01 20:30:04 -0400

    Creating vendor inventory item type model and view.
    WIP: This commit creates the necessary view and model for the vendor inventory item type transaction. The inserting
    into the database still needs to be tested. I was having trouble getting to the view I created so some work still
    needs to be done in order to make sure the vendor inventory item type is getting inserted into the database.

---
+++ model/VendorInventoryItemType.java
@@ -0,0 +1,131 @@
+package model;
+
+//system imports
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+import javax.swing.JFrame;
+
+//project imports
+import exception.InvalidPrimaryKeyException;
+import database.*;
+
+import impresario.IView;
+
+import userInterface.View;
+import userInterface.ViewFactory;
+
+public class VendorInventoryPrice extends EntityBase implements IView 
+{
+	private static final String myTableName = "Vendor-InventoryItemType";
+
+	protected Properties dependencies;
+
+	// GUI Components
+
+	private String updateStatusMessage = "";
+
+
+	// Can also be used to create a NEW Account (if the system it is part of
+	// allows for a new account to be set up)
+	//----------------------------------------------------------
+	public VendorInventoryPrice(Properties props)
+	{
+		super(myTableName);
+
+		setDependencies();
+		persistentState = new Properties();
+		Enumeration allKeys = props.propertyNames();
+		while (allKeys.hasMoreElements() == true)
+		{
+			String nextKey = (String)allKeys.nextElement();
+			String nextValue = props.getProperty(nextKey);
+			if (nextValue != null)
+			{
+				persistentState.setProperty(nextKey, nextValue);
+			}
+		}
+	}
+
+	//-----------------------------------------------------------------------------------
+	private void setDependencies()
+	{
+		dependencies = new Properties();
+	
+		myRegistry.setDependencies(dependencies);
+	}
+
+	//----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("UpdateStatusMessage") == true)
+			return updateStatusMessage;
+
+		return persistentState.getProperty(key);
+	}
+
+	//----------------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+
+		myRegistry.updateSubscribers(key, this);
+	}
+
+	/** Called via the IView relationship */
+	//----------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		stateChangeRequest(key, value);
+	}
+	//-----------------------------------------------------------------------------------
+	public void update()
+	{
+		updateStateInDatabase();
+	}
+	
+	//-----------------------------------------------------------------------------------
+	public void updateStateInDatabase() 
+	{
+		try
+		{
+			Integer vendorInventoryItemTypeId = insertAutoIncrementalPersistentState(mySchema, persistentState);
+			persistentState.setProperty("Id", "" + vendorInventoryItemTypeId.intValue());
+			updateStatusMessage = "Vendor-InventoryItemType data for new vendor : " +  persistentState.getProperty("Id")
+					+ "installed successfully in database!";
+		}
+		catch (SQLException ex)
+		{
+			updateStatusMessage = "Error in installing account data in database!";
+		}
+		//DEBUG System.out.println("updateStateInDatabase " + updateStatusMessage);
+	}
+
+
+	/**
+	 * This method is needed solely to enable the Account information to be displayable in a table
+	 *
+	 */
+	//--------------------------------------------------------------------------
+	public Vector<String> getEntryListView()
+	{
+		Vector<String> v = new Vector<String>();
+
+		v.addElement(persistentState.getProperty("Id"));
+		v.addElement(persistentState.getProperty("VendorId"));
+		v.addElement(persistentState.getProperty("InventoryItemTypeName"));
+		v.addElement(persistentState.getProperty("VendorPrice"));
+		v.addElement(persistentState.getProperty("DateOfLastUpdate"));
+		return v;
+	}
+
+	//-----------------------------------------------------------------------------------
+	protected void initializeSchema(String tableName)
+	{
+		if (mySchema == null)
+		{
+			mySchema = getSchemaInfo(tableName);
+		}
+	}
+
+}
---
+++ userInterface/VendorInventoryPriceView.java
@@ -0,0 +1,226 @@
+package userInterface;
+
+//Imports
+import java.util.Properties;
+import impresario.IModel;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.VBox;
+import javafx.event.Event;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import model.InventoryItemType;
+import model.Vendor;
+import model.VendorInventoryPrice;
+
+import java.time.LocalDate;
+
+public class VendorInventoryPriceView  extends View 
+{
+	
+	private GridPane grid;
+	private TextField priceTF;
+	private Button submitBTN;
+	private Button cancelBTN;
+	private Label priceLBL;
+	private Label messageLBL;
+	
+	// For showing error message
+	protected MessageView statusLog;
+	
+	public VendorInventoryPriceView(IModel model) {
+		super(model, "EnterBookView");
+		
+		VBox container = new VBox(10);
+
+		container.setPadding(new Insets(15, 5, 5, 5));
+
+		// create a Node (Text) for showing the title
+		container.getChildren().add(createTitle());
+
+		// create a Node (GridPane) for showing data entry fields
+		container.getChildren().add(createFormContents());
+		getChildren().add(container);
+
+		populateFields();
+
+		// STEP 0: Be sure you tell your model what keys you are interested in
+		myModel.subscribe("LoginError", this);
+
+	}
+	
+	private void populateFields() {
+		priceTF.setText("");
+		messageLBL.setText("");
+	}
+	
+	private Node createTitle() {		
+		Text titleText = new Text("       Restaurant Inventory Managment         ");
+		titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		titleText.setTextAlignment(TextAlignment.CENTER);
+		titleText.setFill(Color.DARKGREEN);
+		
+	
+		return titleText;
+	}
+	
+	private Node createFormContents() {
+		
+		//CREATE GUI COMPONETS
+		
+		grid = new GridPane();
+		priceTF = new TextField();
+		priceLBL = new Label("Price");
+		messageLBL = new Label();
+		cancelBTN = new Button("Cancel");
+	  	submitBTN = new Button("Submit");
+		
+		//ADD COMPONTES TO GRID.
+		grid.add(priceLBL, 0, 0);				grid.add(priceTF, 1, 0);
+		grid.add(submitBTN, 0, 1);				grid.add(cancelBTN, 1, 1);
+		grid.add(messageLBL, 0, 4);
+		
+		//STYLE GRID.
+		
+    	grid.setAlignment(Pos.CENTER);
+   		grid.setHgap(10);
+    	grid.setVgap(10);
+    	grid.setPadding(new Insets(25, 25, 25, 25));
+    	
+    	//BUTTON HANDLERS.
+    	
+    	//Call process action.
+    	submitBTN.setOnAction(new EventHandler<ActionEvent>() {
+ 		     public void handle(ActionEvent e) {
+ 		     	processAction(e);    
+      	     }
+    	});
+  
+    	//Call process action.
+    	cancelBTN.setOnAction(new EventHandler<ActionEvent>() {
+		     public void handle(ActionEvent e) {
+		     	processAction(e);    
+     	     }
+    	});
+    
+    	//Return the grid.
+    	return grid;
+	}
+	
+	
+	//Method to process all actions.
+	protected void processAction(Event e) {
+		
+		//If it is the cancle button request a cancel state change request.
+		if(e.getSource() == cancelBTN) {
+			myModel.stateChangeRequest("cancel", null);
+		}
+		//If the text field is emtpy.
+		else if(priceTF.getText().isEmpty()) {
+			//Focus on the text field
+			priceTF.requestFocus();
+			//Display message to enter price
+			messageLBL.setText("Please enter price.");
+		}
+		//Check to make sure the number is positive.
+		else if(isDouble(priceTF.getText())) {
+			priceTF.requestFocus();
+			messageLBL.setText("Please enter a positive decimal number for the price.");
+		}
+		else {
+			//Proces the vendorIventoryItemType.
+			
+			//Get the data.
+			String vendorId = Vendor.getSelectedVendorId();
+			//Make sure to set the inventoryItemTypeName in the inventoryItemTypeCollection view.
+			String inventoryItemTypeName = InventoryItemType.getSelectedInventoryItemTypeName();
+			String price = priceTF.getText();
+			String dateOfLastUpdate = LocalDate.now().toString();
+			
+			//Proces the vendor inventory item type.
+			processVendorInventoryItemType(vendorId, inventoryItemTypeName, price, dateOfLastUpdate);
+			
+			//Clear the price box.
+			priceTF.setText("");
+			
+			//Display message.
+			displayMessage("Vendor Inventory Item Type has been entered!");
+		}
+				
+	}
+	
+	//This creates a properties object and inserts it into the database.
+	private void processVendorInventoryItemType(String vendorId, String inventoryItemTypeName, String price, String dateOfLastUpdate) {
+		
+		//Make sure to convery to the right data type of necessary (String to double and so on).
+		
+		//New properties object.
+		Properties props = new Properties();
+		
+		//Set the properties.
+		props.setProperty("VendorId", vendorId);
+		props.setProperty("InventoryItemTypeName", inventoryItemTypeName);
+		props.setProperty("VendorPrice", price);
+		props.setProperty("DateOfLastUpdate", dateOfLastUpdate);
+		
+		//Create the Vendor Inventory Price object.
+		VendorInventoryPrice vip = new VendorInventoryPrice(props);
+		
+		//Save into the database.
+		vip.update();
+		
+		//Debug.
+		System.out.println(vip.toString());
+		
+		//Switch to a screen if you want to.
+		//myModel.stateChangeRequest("VendorInventoryPrice", "");
+	}
+	
+	public void updateState(String key, Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void displayMessage(String message)
+	{
+		statusLog.displayMessage(message);
+	}
+
+	
+	//Method to test is a string is a double.
+	private boolean isDouble (String s) {
+		try {
+			if(Double.parseDouble(s) < 0) {
+				return false;
+			}
+		}
+		catch(NumberFormatException e) {
+			return false;
+		}
+		catch(NullPointerException e) {
+			return false;
+		}
+		
+		return true;
+	}
+			
+			
+}
