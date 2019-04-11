5a55543b73ca5e16561acd8423c38fb5005c6cd6
scottcarpenter28 <33328166+scottcarpenter28@users.noreply.github.com>
2019-04-11 17:19:46 -0400

    Added delete VIIT

--- model/VendorInventoryItemType.java
+++ model/VendorInventoryItemType.java
@@ -48,6 +48,51 @@ public class VendorInventoryItemType extends EntityBase implements IView
 		}
 	}
 
+	public VendorInventoryItemType(String itemTypeName) throws InvalidPrimaryKeyException {
+		super(myTableName);
+		
+		String query = "SELECT * FROM " + myTableName + " WHERE (ItemTypeName = '" + itemTypeName + "')";
+
+		Vector<Properties> allDataRetrieved = getSelectQueryResult(query);
+
+		// You must get one item at least
+		if (allDataRetrieved != null)
+		{
+			int size = allDataRetrieved.size();
+
+			// There should be EXACTLY one item. More than that is an error
+			if (size != 1)
+			{
+				throw new InvalidPrimaryKeyException("Multiple InventoryItemType matching name : "
+					+ size + " found.");
+			}
+			else
+			{
+				// copy all the retrieved data into persistent state
+				Properties retrievedItemData = allDataRetrieved.elementAt(0);
+				persistentState = new Properties();
+
+				Enumeration<?> allKeys = retrievedItemData.propertyNames();
+				while (allKeys.hasMoreElements() == true)
+				{
+					String nextKey = (String)allKeys.nextElement();
+					String nextValue = retrievedItemData.getProperty(nextKey);
+
+					if (nextValue != null)
+					{
+						persistentState.setProperty(nextKey, nextValue);
+					}
+				}
+
+			}
+		}
+		// If no item found for this id, throw an exception
+		else
+		{
+			throw new InvalidPrimaryKeyException("No InventoryItemType matching name : " + itemTypeName);
+		}
+	}
+	
 	//-----------------------------------------------------------------------------------
 	private void setDependencies()
 	{
@@ -102,6 +147,25 @@ public class VendorInventoryItemType extends EntityBase implements IView
 			System.out.println(ex.getMessage());
 		}
 		//DEBUG System.out.println("updateStateInDatabase " + updateStatusMessage);
+	}
+	
+public void delete() {
+		
+		try {
+			
+			if (persistentState.getProperty("ItemTypeName") != null) {
+				
+				Properties whereClause = new Properties();
+				whereClause.setProperty("ItemTypeName",
+						persistentState.getProperty("ItemTypeName"));
+					deletePersistentState(mySchema, whereClause);
+				updateStatusMessage = "InventoryItemType data for name: " + persistentState.getProperty("ItemTypeName") + " delete successfully in database!";
+		
+			}	
+		} catch (SQLException e) {
+			updateStatusMessage = "Error in deleting InventoryItemType data in database!";
+		}
+		
 	}
 
 
